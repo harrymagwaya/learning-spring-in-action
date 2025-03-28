6a54064b3d3c0e69b1ccaccd6e3b0b71bc5fdd9a
mwijuka <mwijukaharoid@gmail.com>
2025-03-28 20:40:40 +0300

    added tests and validation for order form

--- src/test/java/com/taco/taco/OrderControllerTest.java
+++ src/test/java/com/taco/taco/OrderControllerTest.java
@@ -1,32 +1,33 @@
 package com.taco.taco;
 
-import org.junit.jupiter.api.Test;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import javax.print.attribute.standard.Media;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-import com.taco.taco.controllers.HomeController;
+import com.taco.taco.controllers.OrderController;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
-public class HomeControllerTest {
-
+@WebMvcTest(OrderController.class)
+public class OrderControllerTest {
+    
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void testHomePage()throws Exception{
-        mockMvc.perform(get("/"))
+    public void testOrderPage()throws Exception{
+        mockMvc.perform(get("/orders"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("home"))
-                .andExpect(content().contentType("text/html; charset=utf8"));   
+                .andExpect(view().name("orderForm"))
+                .andExpect(content().contentType("text/html; charset=utf8"));
     }
 }
