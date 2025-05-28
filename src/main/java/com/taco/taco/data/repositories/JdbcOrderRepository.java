372ec205c220f7db7ea12729a80759d31545eab8
mwijuka <mwijukaharoid@gmail.com>
2025-05-28 12:01:50 +0300

    added more functions to jdbc order

--- src/main/java/com/taco/taco/data/repositories/JdbcOrderRepository.java
+++ src/main/java/com/taco/taco/data/repositories/JdbcOrderRepository.java
@@ -1,9 +1,16 @@
 package com.taco.taco.data.repositories;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taco.taco.data.Order;
+import com.taco.taco.data.Taco;
 import com.taco.taco.data.models.OrderRepository;
 
 public class JdbcOrderRepository  implements OrderRepository{
@@ -12,11 +19,44 @@ public class JdbcOrderRepository  implements OrderRepository{
     private SimpleJdbcInsert orderTacoInserter;
     private ObjectMapper objectMapper;
 
+    public JdbcOrderRepository(JdbcTemplate jdbc){
+        this.ordeInserter = new SimpleJdbcInsert(jdbc).withTableName("Taco_Order").usingGeneratedKeyColumns("id");
+        this.orderTacoInserter = new SimpleJdbcInsert(jdbc).withTableName("Taco_Order_Tacos");
+
+        this.objectMapper = new ObjectMapper();
+    }
+
 
     @Override
     public Order save(Order order) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        order.setPlacedAt(new Date());
+        long orderid = saveOrderDetails(order);
+        order.setId(orderid);
+
+        List<Taco> tacos = order.getTacos();
+
+        for (Taco taco : tacos) {
+            saveTacoOrder(taco, orderid);
+        }
+
+        return order;
+    }
+    
+    private long saveOrderDetails(Order order){
+        
+        Map <String, Object> values = objectMapper.convertValue(order, Map.class);
+        values.put("placedAt", order.getPlacedAt());
+
+        long orderId = ordeInserter.executeAndReturnKey(values)
+                                    .longValue();
+        return orderId;
+    }
+
+    private void saveTacoToOrder(Taco taco, long orderId){
+        Map <String , Object> values = new HashMap<>();
+        values.put("tacoOrder", orderId);
+        values.put("taco", taco.getId());
+        ordeInserter.execute(values);
     }
     
 }
